dbb7ddac2769ab6331d6fe04c9bab947a59dd90f
Yvyel <102757499+Yvyel@users.noreply.github.com>
2022-04-26 14:55:50 -0700

    Add files via upload

---
+++ floating-numbers.py
@@ -0,0 +1,26 @@
+# Store input numbers
+x = '15'
+y = '3'
+
+# Add two numbers
+sum = float(x) + float(y)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(x, y, sum))
+
+
+# Store input numbers
+a = 25
+b = 32
+
+# Subtract two numbers
+diff = a - b
+
+# Display the difference
+print(diff)
+
+# Multiply one float and one integer
+prod = float(x) * b
+
+# Display the product
+print(prod)
